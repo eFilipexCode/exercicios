ad456a85617031aede09872920eaebc2da9f48ad
Emanuel Filipe <efilipecode@gmail.com>
2021-03-21 13:05:11 -0300

    Exercício 2-1

--- 2/2-1.js
+++ 2/2-1.js
@@ -1,1 +1,23 @@
-//Primeiro exercício
+// Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor 
+//que C
+
+function checkNumbers(a, b, c) {
+    if (a + b < c) {
+        printNumber(`${a} + ${b} < ${c}`);
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function printNumber(string) {
+    console.log(string);
+};
+
+console.log(!!checkNumbers(2, 3, 6));
+console.log(!!checkNumbers(6, 9, 18));
+console.log(!!checkNumbers(123, 400, 960));
+
+console.log(!!checkNumbers(3, 4, 2));
+console.log(!!checkNumbers(3, 5, 1));
+console.log(!!checkNumbers(0, 4, 2));
