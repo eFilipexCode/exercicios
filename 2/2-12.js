bdd9ebc8a63c60ac397d8648c14518adab1e5823
Emanuel Filipe <efilipecode@gmail.com>
2021-04-08 15:30:54 -0300

    Update 2-12.js

--- 2/2-12.js
+++ 2/2-12.js
@@ -6,34 +6,27 @@
 //mensagem 'Aprovado' se o conceito for A, B ou C, e 'Reprovado' se o conceito for D ou E. 
 //Média de aproveitamento Conceito 
 
-const notes = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-];
+const notes = {
+    A: "A",
+    B: "B",
+    C: "C",
+    D: "D",
+    E: "E",
+};
 
 function handleNotes(note) {
     if (note >= 90)
-        return 0
+        return notes.A
     else if (note >= 75 && note < 90)
-        return 1
+        return notes.B
     else if (note >= 60 && note < 75)
-        return 2
+        return notes.C
     else if (note >= 40 && note < 60)
-        return 3
+        return notes.D
     else if (note < 40)
-        return 4
+        return notes.C
 };
 
 const calcMedia = (a, b, c, mediaExercises) => (a + (b * 2) + (c * 3) + mediaExercises) / 7;
 
-function printMessage(id, a, b, c, mediaExercises, calcMedia, handleNotes) {
-    const value = calcMedia(a, b, c, mediaExercises);
-    const status = handleNotes(value);
-
-    console.log(`${id} - ${a} ${b} ${c}. Media: ${mediaExercises}. Media de Aproveitamento: ${value}. Conceito: ${notes[status]}`)
-};
-
-printMessage(1, 1, 2, 3, 2,calcMedia, handleNotes);
+console.log(habdleNotes(calcMedia(1, 1, 1, 1)) === notes.E);
